9f0690e3772043276ec588492473b84416895a74
ShashwatMishra0099 <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-04-09 19:09:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,11 @@ def add_member(update: Update, context: CallbackContext):
         # Add member to the group
         try:
             chat_id = update.message.chat_id
-            context.bot.bot_data['admins'] = context.bot.get_chat_administrators(chat_id)
-            context.bot.bot_data['creator'] = context.bot.get_chat(chat_id).get('pinned_message').author
-            if update.message.from_user.id in context.bot.bot_data['admins'] or update.message.from_user.id == context.bot.bot_data['creator']:
-                context.bot.bot_data['users'] = context.bot.get_chat_members_count(chat_id)
-                if context.bot.bot_data['users'] + len(context.args) > 100:
+            admins = context.bot.get_chat_administrators(chat_id)
+            creator = context.bot.get_chat(chat_id).get('pinned_message').author
+            if update.message.from_user.id in [admin.user.id for admin in admins] or update.message.from_user.id == creator.id:
+                users_count = context.bot.get_chat_members_count(chat_id)
+                if users_count + len(context.args) > 100:
                     update.message.reply_text(f"Failed to add member {user_info}: The group limit of 100 members exceeded.")
                     return
                 context.bot.add_chat_members(chat_id=chat_id, user_ids=[user_info])
